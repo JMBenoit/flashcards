7ba5caa53c3ebbf2077e41ccb286cdb7f9e30ad1
Justin Benoit <JMBenoit18@gmail.com>
2020-03-24 16:09:53 -0400

    added ability to hide and show known flashcards

--- js/app.js
+++ js/app.js
@@ -3,15 +3,18 @@ function $(value)
   return document.getElementById(value);
 }
 
-function addAnswerToggle()
+function addButtonToggles()
 {
   let flashcards = document.getElementsByClassName('flashcard');
   let lastCard = flashcards[flashcards.length - 1];
   let lastCardP = lastCard.getElementsByTagName('p')[0];
-  let lastCardButton = lastCard.getElementsByTagName('button')[0];
-  console.log(lastCardP);
-  lastCardButton.addEventListener('click', function(){
+  let lastCardShowButton = lastCard.getElementsByTagName('button')[0];
+  lastCardShowButton.addEventListener('click', function(){
     lastCardP.classList.toggle('hide');
+  });
+  let lastCardKnowButton = lastCard.getElementsByTagName('button')[1];
+  lastCardKnowButton.addEventListener('click', function(){
+    lastCard.classList.toggle('hide');
   });
 }
 
@@ -19,13 +22,19 @@ $('add').addEventListener('click', function(){
   let newQuestion = $('question').value;
   let newAnswer = $('answer').value;
   if (newQuestion.length > 0 && newAnswer.length > 0) {
-    let newCard = '<div class="flashcard"><h3>' + newQuestion + '</h3><p class="hide">' + newAnswer + '</p><button type="button" id="show" onclick="">Show/Hide Answer</button></div>'
+    let newCard = '<div class="flashcard"><h3>' + newQuestion + '</h3><p class="hide">' + newAnswer + '</p><button type="button" id="show" onclick="">Show/Hide Answer</button><br><button type="button" id="know">I know it</button></div>'
     $('body').insertAdjacentHTML('beforeend', newCard);
-    addAnswerToggle();
+    addButtonToggles();
     $('question').value = '';
     $('answer').value = '';
   }
-  else {
+  else {}
+});
 
-  }
+$('showKnown').addEventListener('click', function(){
+  let flashcards = document.getElementsByClassName('flashcard');
+  for (i = 0; i < flashcards.length; i++){
+    flashcards[i].classList.remove('hide');
+  };
+
 });
